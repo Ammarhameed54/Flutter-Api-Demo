95d205396ec72bebd3f56969e733bf9d89149699
Ammar Hameed <f21bdats1m02054@iub.edu.pk>
2024-06-26 03:33:20 -0700

    Commit

---
+++ lib/home.dart
@@ -0,0 +1,129 @@
+import 'dart:convert';
+import 'package:apidemo/models/usermodel.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<UserModel> userList = [];
+
+  Future<List<UserModel>> userApi() async {
+    final response =
+        await http.get(Uri.parse("https://jsonplaceholder.typicode.com/users"));
+    var data = jsonDecode(response.body.toString());
+    if (response.statusCode == 200) {
+      for (var i in data) {
+        userList.add(UserModel.fromJson(i));
+      }
+      return userList;
+    } else {
+      return userList;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("User API App"),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+      ),
+      body: FutureBuilder(
+        future: userApi(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            return ListView.builder(
+              itemCount: userList.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Card(
+                    elevation: 5,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            userList[index].name.toString(),
+                            style: const TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black87,
+                            ),
+                          ),
+                          const SizedBox(height: 8),
+                          Text(
+                            'Username: ${userList[index].username.toString()}',
+                            style: const TextStyle(
+                              fontSize: 16,
+                              color: Colors.blueAccent,
+                            ),
+                          ),
+                          const SizedBox(height: 8),
+                          Text(
+                            'Email: ${userList[index].email.toString()}',
+                            style: const TextStyle(
+                              fontSize: 16,
+                              color: Colors.black54,
+                            ),
+                          ),
+                          const SizedBox(height: 8),
+                          Text(
+                            'City: ${userList[index].address!.city.toString()}',
+                            style: const TextStyle(
+                              fontSize: 16,
+                              color: Colors.black54,
+                            ),
+                          ),
+                          const SizedBox(height: 8),
+                          Text(
+                            'Phone: ${userList[index].phone.toString()}',
+                            style: const TextStyle(
+                              fontSize: 16,
+                              color: Colors.black54,
+                            ),
+                          ),
+                          const SizedBox(height: 8),
+                          Text(
+                            'Website: ${userList[index].website.toString()}',
+                            style: const TextStyle(
+                              fontSize: 16,
+                              color: Colors.black54,
+                            ),
+                          ),
+                          const SizedBox(height: 8),
+                          Text(
+                            'Company: ${userList[index].company!.name.toString()}',
+                            style: const TextStyle(
+                              fontSize: 16,
+                              color: Colors.black54,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            );
+          }
+        },
+      ),
+    );
+  }
+}
